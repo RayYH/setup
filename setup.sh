c3e795cc3b105f25cb394f6ac841759aa8d16d1a
rayyh <rayyounghong@gmail.com>
2020-11-22 20:27:21 +0800

    fix

--- setup.sh
+++ setup.sh
@@ -12,11 +12,4 @@ if [[ -z "$SETUP_CUSTOM" ]]; then
     SETUP_CUSTOM="$SETUP/custom"
 fi
 
-is_plugin() {
-  local base_dir=$1
-  local name=$2
-  test -f $base_dir/plugins/$name/$name.plugin.sh \
-    || test -f $base_dir/plugins/$name/_$name
-}
-
 # use $TERM_PROGRAM to check whether is in Terminal.app or in iTerm2
