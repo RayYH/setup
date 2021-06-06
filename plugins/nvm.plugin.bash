11b75581b627a70b3afc3aa492e01ba076370cbc
rayyh <rayyounghong@gmail.com>
2021-06-06 14:30:54 +0800

    Optimize nvm load && proxy settings

--- plugins/nvm.plugin.bash
+++ plugins/nvm.plugin.bash
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+NODE_GLOBALS=($(find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq))
+NODE_GLOBALS+=(node nvm yarn)
+
+_load_nvm() {
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+}
+
+for cmd in "${NODE_GLOBALS[@]}"; do
+  eval "function ${cmd}(){ unset -f ${NODE_GLOBALS[*]}; _load_nvm; unset -f _load_nvm; ${cmd} \$@; }"
+done
+unset cmd NODE_GLOBALS
+
+export PATH="$PATH:$HOME/.yarn/bin"
--- plugins/proxy.plugin.bash
+++ plugins/proxy.plugin.bash
@@ -8,6 +8,10 @@ function set_p() {
   export ftp_proxy=http://127.0.0.1:${port}
   export http_proxy=http://127.0.0.1:${port}
   export https_proxy=http://127.0.0.1:${port}
+  export HTTP_PROXY=${http_proxy}
+  export HTTPS_PROXY=${https_proxy}
+  # see https://minikube.sigs.k8s.io/docs/handbook/vpn_and_proxy/
+  export NO_PROXY=localhost,127.0.0.1,10.96.0.0/12,192.168.99.0/24,192.168.39.0/24
 }
 
 function unset_p() {
@@ -15,4 +19,7 @@ function unset_p() {
   unset ftp_proxy
   unset http_proxy
   unset https_proxy
+  unset HTTP_PROXY
+  unset HTTPS_PROXY
+  unset NO_PROXY
 }
