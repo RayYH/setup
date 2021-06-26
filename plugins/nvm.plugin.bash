a8b0619b3cbdec339a006011d8dec9be1847bebb
rayyh <rayyounghong@gmail.com>
2021-06-26 13:58:38 +0800

    Check nvm

--- plugins/nvm.plugin.bash
+++ plugins/nvm.plugin.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 export NVM_DIR="$HOME/.nvm"
+
+[ -d "$NVM_DIR" ] || return
+
 #[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 #[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
