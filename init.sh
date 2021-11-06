2b16a2d3b8907a1ee68554bca6ef2c56d5193126
rayyh <rayyounghong@gmail.com>
2021-11-06 19:45:13 +0800

    fix: init.sh

--- init.sh
+++ init.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
-    exit
-fi
-
 SCRIPT_PATH="$(
     cd -- "$(dirname "$0")" || exit >/dev/null 2>&1
     pwd -P
 )"
-WORKING_DIR="$(dirname "$SCRIPT_PATH")"
-
-echo "$WORKING_DIR"
+WORKING_DIR="$SCRIPT_PATH"
 
 # load helpers
-source "$WORKING_DIR/init/common.bash"
+source "$WORKING_DIR/init/helper.bash"
 source "$WORKING_DIR/init/logger.bash"
 source "$WORKING_DIR/init/packages.bash"
 
-info "this script will setup your macos (need root privileges)"
+info "This script will setup your macos development environment, please be patient (some script will need root privileges)..."
 
 # xcode-select
 sudo xcode-select --install 2 &>/dev/null
@@ -31,7 +24,5 @@ init_js_development
 init_cpp_development
 init_go_development
 install_quick_plugins
-
-
 
 #install_guis
