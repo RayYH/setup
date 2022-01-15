f01b40f8e215b52de56ae10a8b138332d0d8720a
rayyh <rayyounghong@gmail.com>
2022-01-15 13:51:33 +0800

    ci: shellcheck and yml format

--- profile/plugins.bash
+++ profile/plugins.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# shellcheck disable=SC1091
 if type brew &>/dev/null; then
   # autojump
   [ -f "$(brew --prefix)"/etc/profile.d/autojump.sh ] && . "$(brew --prefix)"/etc/profile.d/autojump.sh
