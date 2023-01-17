bcf5a306e0f5eb1bafdee79c56053de080784e23
rayyh <rayyounghong@gmail.com>
2023-01-17 17:18:29 +0800

    fix: aliases

--- profile/ray.bash
+++ profile/ray.bash
@@ -6,19 +6,9 @@ alias obsi="cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents" # Obsid
 
 CODE_DIR="$HOME/Code"
 alias p="cd $CODE_DIR"
-alias pc="cd $CODE_DIR/c"
-alias pcpp="cd $CODE_DIR/cpp"
-alias pgo="cd $CODE_DIR/go"
-alias pjava="cd $CODE_DIR/java"
-alias ppython="cd $CODE_DIR/python"
-alias pjs="cd $CODE_DIR/javascript"
-alias pphp="cd $CODE_DIR/php"
-alias prust="cd $CODE_DIR/rust"
-alias pshell="cd $CODE_DIR/shell"
-alias pwork="cd $CODE_DIR/work"
-alias pself="cd $CODE_DIR/self"
-alias pray="cd $CODE_DIR/rayyh"
-alias plily="cd $CODE_DIR/lily"
+alias pwo="cd $CODE_DIR/work"
+alias ppr="cd $CODE_DIR/projects"
+alias psi="cd $CODE_DIR/simple"
 
 ## kitty alias
 #alias icat="kitty +kitten icat"
