efec1916a26aa611754f0ecd3af41b87f303bf93
rayyh <rayyounghong@gmail.com>
2023-03-02 11:15:53 +0800

    fix: completion should be sourced manually

--- profile/completions.bash
+++ profile/completions.bash
@@ -7,6 +7,9 @@ if command -v brew &>/dev/null && [ -r "$(brew --prefix)/etc/profile.d/bash_comp
   export BASH_COMPLETION_COMPAT_DIR
   # shellcheck disable=SC1090
   source "$(brew --prefix)/etc/profile.d/bash_completion.sh"
+  for file in "$BASH_COMPLETION_COMPAT_DIR/*"; do
+    [[ -x $file ]] && source "$file"
+  done
 elif [ -f /etc/bash_completion ]; then
   # shellcheck disable=SC1091
   source /etc/bash_completion
