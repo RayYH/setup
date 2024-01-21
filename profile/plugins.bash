38c11f6510a64c8355e90b71b555a7f0f4be1a54
rayyh <rayyounghong@gmail.com>
2024-01-21 12:15:01 +0800

    path: update

--- profile/plugins.bash
+++ profile/plugins.bash
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1091
 if type brew &>/dev/null; then
-  # autojump
-  [ -f "$(brew --prefix)"/etc/profile.d/autojump.sh ] && . "$(brew --prefix)"/etc/profile.d/autojump.sh
-  # asdf
-  [ -f "$(brew --prefix)/opt/asdf/libexec/asdf.sh" ] && . "$(brew --prefix)/opt/asdf/libexec/asdf.sh"
   # fzf
   [ -f "$HOME/.fzf.bash" ] && source "$HOME/.fzf.bash"
 fi
@@ -15,4 +11,18 @@ if [ -d "${HOME}/.sdkman" ]; then
   #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
   export SDKMAN_DIR="$HOME/.sdkman"
   [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+fi
+
+if type brew &>/dev/null
+then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+  then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+    do
+      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+    done
+  fi
 fi
