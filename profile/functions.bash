e049e295287ccb333adb6f450935308ab0b3793e
rayyh <rayyounghong@gmail.com>
2024-02-20 10:35:56 +0800

    feat: add yazi support

--- profile/functions.bash
+++ profile/functions.bash
@@ -521,3 +521,13 @@ function mman() {
   man "$@" 2>/dev/null
 }
 alias man="mman"
+
+# yazi support: https://yazi-rs.github.io/docs/quick-start
+function ya() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
